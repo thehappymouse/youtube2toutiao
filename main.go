227108792d1c6540e32187c54d1cee57a3e0dece
王伟 <thehappymouse@gmail.com>
2020-11-10 16:58:12 +0800

    使用 mod

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"os"
-
-	"dali.cc/toutiao/admin"
-	"dali.cc/toutiao/downloader"
-	"dali.cc/toutiao/tools"
-	"dali.cc/toutiao/translator"
-	"github.com/gpmgo/gopm/modules/log"
+	"toutiao/admin"
+	"toutiao/downloader"
+	"toutiao/tools"
+	"toutiao/translator"
 )
 
 type A struct {
@@ -28,13 +27,18 @@ var dao = &translator.YouDao{
 	AppKey: "6a0f0aec8e860c65",
 	SecKey: "vTrsGcDDmD0X6RIUUpCi0oEGazF30BOz",
 }
-var id = flag.String("url", "", "请输入一个 youtube 地址")
+
+var id = flag.String("url", "BV1K741137fi", "请输入一个 youtube 地址")
 
 func main() {
 	flag.Parse()
+	if *id == "" {
+		log.Fatal().Msg("请输入要下载的 url， --url=Szw_1B-IBcs")
+	}
+
 	ok, video := downloader.Download(*id)
 	if !ok {
-		log.Error("下载源文件出错")
+		log.Error().Msg("下载源文件出错")
 		os.Exit(1)
 	}
 
@@ -53,19 +57,19 @@ func main() {
 
 	md5Resp := admin.Md5Check(video.Md5)
 	if !md5Resp.IsUniq {
-		log.Warn("Video Already use in [%s]", md5Resp.Data)
+		log.Warn().Msgf("Video Already use in [%s]", md5Resp.Data)
 		return
 	}
 
 	videoapi := admin.VideoApi()
 
-	log.Warn("video/api: %v", videoapi)
+	log.Warn().Msgf("video/api: %v", videoapi)
 
 	admin.VideoLogStart(&video, videoapi)
 
 	uploadResponse := admin.VideoUpload(&video, videoapi)
 
-	log.Warn("uploadResponse: %v", uploadResponse)
+	log.Warn().Msgf("uploadResponse: %v", uploadResponse)
 
 	admin.VideoLogSueecss(uploadResponse, videoapi, &video)
 
